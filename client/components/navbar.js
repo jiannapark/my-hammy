34a4f235e5740b4d0b8b76fdff5124be22ab939a
Jianna Park <jiannajihyunpark@gmail.com>
2020-09-05 11:08:30 -0400

    change title

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>My Hammy</h1>
     <nav>
       {isLoggedIn ? (
         <div>
